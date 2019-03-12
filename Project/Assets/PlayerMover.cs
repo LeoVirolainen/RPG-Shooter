2623ca03c6aaa12bad08daad1cb54d3a976e7a22
OPISTO\leo.virolainen <virolainen.leo@gmail.com>
2019-03-12 13:29:01 +0200

    move but not diagonal :(

---
+++ Project/Assets/PlayerMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+    public Rigidbody2D rb2D;
+    public float walkSpeed = 2.0f;
+
+
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.W)) {
+            rb2D.MovePosition(rb2D.position + Vector2.up * Time.deltaTime * walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            rb2D.MovePosition(rb2D.position + Vector2.down * Time.deltaTime * walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            rb2D.MovePosition(rb2D.position + Vector2.right * Time.deltaTime * walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            rb2D.MovePosition(rb2D.position + Vector2.left * Time.deltaTime * walkSpeed);
+        }
+    }
+}
