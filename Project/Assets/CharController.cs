d4ca2a162fbb15d0562a35c1d44a1de1db45fc35
OPISTO\leo.virolainen <virolainen.leo@gmail.com>
2019-03-28 13:00:14 +0200

    redone scene, started animations

---
+++ Project/Assets/CharController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+    public float moveSpeed = 5f;
+
+    public Vector3 forward;
+    public Vector3 right;
+
+    public Animator animr;
+
+    void Start() {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
+    }
+
+    void Update() {
+        if (Input.anyKey) {
+            Move();
+        }
+    }
+
+    private void Move() {
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        //Vector3.ClampMagnitude(rightMovement + upMovement, 1.0f) * moveSpeed;
+
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+
+        animr.Play("Walk");
+    }
+
+}
---
+++ Project/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+    public Rigidbody rb;
+    public float walkSpeed = 2.0f;
+
+
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.W)) {
+            rb.MovePosition(rb.position + Vector3.forward * Time.deltaTime * walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            rb.MovePosition(rb.position + Vector3.back * Time.deltaTime * walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            rb.MovePosition(rb.position + Vector3.right * Time.deltaTime * walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            rb.MovePosition(rb.position + Vector3.left * Time.deltaTime * walkSpeed);
+        }
+    }
+}
