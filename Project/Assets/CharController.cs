2241a534112cb3a833850dbc2c26210c2afa3cd6
OPISTO\leo.virolainen <virolainen.leo@gmail.com>
2019-04-02 11:08:58 +0300

    dodgy janky movement anim

--- Project/Assets/CharController.cs
+++ Project/Assets/CharController.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharController : MonoBehaviour {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 4f;
 
     public Vector3 forward;
     public Vector3 right;
 
     public Animator animr;
 
+
     void Start() {
         forward = Camera.main.transform.forward;
         forward.y = 0;
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
-
     }
 
     void Update() {
         if (Input.anyKey) {
             Move();
+            if (Input.anyKeyDown) {
+                animr.Play("Walk");
+            }
+        }
+        if (!Input.anyKey) {
+            animr.Play("Idle");
         }
     }
 
@@ -34,8 +40,5 @@ public class CharController : MonoBehaviour {
         transform.forward = heading;
         transform.position += rightMovement;
         transform.position += upMovement;
-
-        animr.Play("Walk");
     }
-
 }
